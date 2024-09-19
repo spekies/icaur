f924583f02f72190e61785e8a3e2969c14436359
spekies <179425982+spekies@users.noreply.github.com>
2024-09-18 22:59:01 -0400

    Update main.c

--- main.c
+++ main.c
@@ -2,39 +2,95 @@
 #include <stdlib.h>
 #include <string.h>
 #include <curl/curl.h>
+#include <cjson/cJSON.h>
 
 #define AUR_URL "https://aur.archlinux.org/rpc.php"
 #define MAX_BUFFER 1024
 
+// Function to write response data to a buffer
 size_t write_callback(void *ptr, size_t size, size_t nmemb, void *userdata) {
     strncat(userdata, ptr, size * nmemb);
     return size * nmemb;
 }
 
+// Function to download a file
+void download_file(const char *url, const char *filename) {
+    CURL *curl;
+    FILE *fp;
+    CURLcode res;
+
+    curl = curl_easy_init();
+    if (curl) {
+        fp = fopen(filename, "wb");
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+        curl_easy_setopt(curl, CURLOPT_WRITEFILE, fp);
+
+        // Perform the request
+        res = curl_easy_perform(curl);
+        
+        // Check for errors
+        if (res != CURLE_OK) {
+            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+        }
+
+        fclose(fp);
+        curl_easy_cleanup(curl);
+    }
+}
+
+// Function to search for a package in the AUR
 void search_package(const char *package_name) {
     CURL *curl;
     CURLcode res;
     char buffer[MAX_BUFFER] = {0};
 
+    // Initialize CURL
     curl_global_init(CURL_GLOBAL_DEFAULT);
     curl = curl_easy_init();
     
     if(curl) {
+        // Prepare the URL for the search
         char url[MAX_BUFFER];
-        snprintf(url, sizeof(url), "%s?type=search&arg=%s", AUR_URL, package_name);
+        snprintf(url, sizeof(url), "%s?v=5&type=search&arg=%s", AUR_URL, package_name);
 
         curl_easy_setopt(curl, CURLOPT_URL, url);
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, buffer);
         
+        // Perform the request
         res = curl_easy_perform(curl);
         
+        // Check for errors
         if(res != CURLE_OK) {
             fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
         } else {
             printf("Response:\n%s\n", buffer);
+            // Parse JSON response
+            cJSON *json = cJSON_Parse(buffer);
+            if (json) {
+                cJSON *results = cJSON_GetObjectItem(json, "results");
+                if (cJSON_GetArraySize(results) > 0) {
+                    // Get the first result
+                    cJSON *pkg = cJSON_GetArrayItem(results, 0);
+                    const char *pkgbase = cJSON_GetObjectItem(pkg, "Name")->valuestring;
+
+                    // Construct the URL to download the PKGBUILD
+                    char pkgbuild_url[MAX_BUFFER];
+                    snprintf(pkgbuild_url, sizeof(pkgbuild_url), "https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=%s", pkgbase);
+
+                    // Download the PKGBUILD
+                    printf("Downloading PKGBUILD for %s...\n", pkgbase);
+                    download_file(pkgbuild_url, "PKGBUILD");
+                } else {
+                    printf("No packages found for '%s'.\n", package_name);
+                }
+                cJSON_Delete(json);
+            } else {
+                printf("Error parsing JSON response.\n");
+            }
         }
         
+        // Cleanup
         curl_easy_cleanup(curl);
     }
 
